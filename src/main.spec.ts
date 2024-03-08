1120aaafd1722f1b1f81dc0b4e984409381e817a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-08 16:00:57 +0000

    green: when 07:45 I should study

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -13,6 +13,7 @@ class MorningRoutine {
 
   whatShouldIDoNow(): string {
     if (!this.currentTime) return "Do exercise"
+    if (this.currentTime?.getTime() > new Date("2024-3-8 07:30:00").getTime()) return "Study"
     if (this.currentTime?.getTime() > new Date("2024-3-8 07:00:00").getTime()) return "Read"
     return "Do exercise"
   }
